7d8f353f8a8699b67d46a32bf60363016d869ace
mvoisin78 <mvoisin@hotmail.com>
2020-10-07 11:28:52 +0200

    Dl video

--- app/src/main/java/com/example/javaembarque/ServerActivity.java
+++ app/src/main/java/com/example/javaembarque/ServerActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.app.DownloadManager;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -32,11 +35,17 @@ public class ServerActivity extends AppCompatActivity {
     private VideoView videoView;
     private Button btn_download;
     BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+    String url;
+    ProgressBar mProgressBar;
+    EditText texte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        texte = (EditText) findViewById(R.id.uri);
+        final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
 
        /* String toastText;
@@ -51,20 +60,55 @@ public class ServerActivity extends AppCompatActivity {
 
         btn_download = (Button) findViewById(R.id.download_btn);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
         btn_download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String url = "https://ia800201.us.archive.org/22/items/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet.mp4";
-                // get download service and enqueue file
-                DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                url = texte.getText().toString();
+                mProgressBar.setProgress(0);
+                mProgressBar.setMax(100);
                 DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                new Thread(new Runnable() {
 
+                    @Override
+                    public void run() {
+
+                        boolean downloading = true;
+
+                        while (downloading) {
+
+                            DownloadManager.Query q = new DownloadManager.Query();
+
+                            Cursor cursor = manager.query(q);
+                            cursor.moveToFirst();
+                            int bytes_downloaded = cursor.getInt(cursor
+                                    .getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                            int bytes_total = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+
+                            if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                                downloading = false;
+                            }
+
+                            final int dl_progress = (int) ((double)bytes_downloaded / (double)bytes_total * 100f);
+
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress((int) dl_progress);
+                                }
+                            });
+
+                            cursor.close();
+                        }
+
+                    }
+                }).start();
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
                 Long reference = manager.enqueue(request);
 
             }
         });
     }
-
 }
